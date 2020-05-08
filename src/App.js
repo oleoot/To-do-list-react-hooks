22c68d9948b99114b48f8223ae9265eee77b4962
oleoot <oleoot28@gmail.com>
2020-05-08 12:42:21 +0300

    Button for completing todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import './App.css'
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className="todo">
+    <div style={{ textDecoration: todo.isComleted ? 'line-through' : '' }} className="todo">
       {todo.text}
+      <div>
+        <button onClick={() => { completeTodo(index) }}>Complete</button>
+      </div>
     </div>
   )
 }
@@ -20,7 +23,7 @@ function TodoForm({ addTodo }) {
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" className="input" value={value} onChange={(e) => { setValue(e.target.value) }} />
+      <input type="text" className="input" value={value} placeholder={"Add todo..."} onChange={(e) => { setValue(e.target.value) }} />
     </form>
   )
 }
@@ -47,11 +50,18 @@ function App() {
   }
 
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isComleted = true;
+    setTodos(newTodos)
+  }
+
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>

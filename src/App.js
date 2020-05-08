da08fea3ee35d2b3da9a437e4d1563551b21ae6f
oleoot <oleoot28@gmail.com>
2020-05-08 12:23:43 +0300

    displaying todos on the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+
+function Todo({ todo, index }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
+}
+
+
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about react',
+      isComleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isComleted: false
+    },
+    {
+      text: 'Build really cool todo app',
+      isComleted: false
+    },
+  ]);
+  console.log(todos)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
